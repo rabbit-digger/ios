39f3ee11218ec71966d6ad6305fd08878de54b70
spacemeowx2 <spacemeowx2@gmail.com>
2022-03-07 00:51:39 +0800

    build: add xcode project

---
+++ rdp-ios/rdp-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rdp-ios
+//
+//  Created by space on 2022/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rdp-ios/rdp-ios/rdp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  rdp_iosApp.swift
+//  rdp-ios
+//
+//  Created by space on 2022/3/6.
+//
+
+import SwiftUI
+
+@main
+struct rdp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
